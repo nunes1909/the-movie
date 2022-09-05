ff1b939cbf8892181ceec8e1027fc836df7ddf05
Gabriel <gnunes1909@gmail.com>
2022-09-05 19:55:23 -0300

    TM-7 feat: Criando layouts

--- remote/src/main/java/com/gabriel/remote/serie/dataSource/GetSeriesDataSourceImpl.kt
+++ remote/src/main/java/com/gabriel/remote/serie/dataSource/GetSeriesDataSourceImpl.kt
@@ -5,16 +5,16 @@ import com.gabriel.data.serie.model.SerieData
 import com.gabriel.remote.serie.mapper.SerieRemoteMapper
 import com.gabriel.remote.serie.service.SeriesService
 
-class GetSeriesDataSourceImpl(
-    private val api: SeriesService,
-    private val mapperSerie: SerieRemoteMapper,
-    // Criar detail mapper
-) : GetSeriesDataSource {
-    override suspend fun getAllSeries(): List<SerieData> {
-
-    }
-
-    override suspend fun getDetailSerie(serieId: Int): SerieData {
-
-    }
-}
+//class GetSeriesDataSourceImpl(
+//    private val api: SeriesService,
+//    private val mapperSerie: SerieRemoteMapper,
+//    // Criar detail mapper
+//) : GetSeriesDataSource {
+//    override suspend fun getAllSeries(): List<SerieData> {
+//
+//    }
+//
+//    override suspend fun getDetailSerie(serieId: Int): SerieData {
+//
+//    }
+//}
