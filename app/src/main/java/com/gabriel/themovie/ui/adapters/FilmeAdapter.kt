f3b07267047e11116dc71c74eb71649314e44b46
Gabriel <gnunes1909@gmail.com>
2022-09-07 15:26:55 -0300

    TM-15 feat: Criando Adapter de Filmes e Series

---
+++ app/src/main/java/com/gabriel/themovie/ui/adapters/FilmeAdapter.kt
@@ -0,0 +1,66 @@
+package com.gabriel.themovie.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.gabriel.themovie.databinding.ItemListBinding
+import com.gabriel.themovie.model.filme.model.FilmeView
+import com.gabriel.themovie.util.constants.ConstantsView.BASE_URL_IMAGES
+
+class FilmeAdapter : RecyclerView.Adapter<FilmeAdapter.FilmeViewHolder>() {
+
+    inner class FilmeViewHolder(val binding: ItemListBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    private val differConfig = object : DiffUtil.ItemCallback<FilmeView>() {
+        override fun areItemsTheSame(oldItem: FilmeView, newItem: FilmeView): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+
+        override fun areContentsTheSame(oldItem: FilmeView, newItem: FilmeView): Boolean {
+            return oldItem.id == newItem.id &&
+                    oldItem.title == newItem.title &&
+                    oldItem.description == newItem.description &&
+                    oldItem.banner == newItem.banner &&
+                    oldItem.background == newItem.background &&
+                    oldItem.nota == newItem.nota &&
+                    oldItem.favorito == newItem.favorito
+        }
+    }
+
+    private val differ = AsyncListDiffer(this, differConfig)
+
+    var filmesList: List<FilmeView>
+        get() = differ.currentList
+        set(value) = differ.submitList(value)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FilmeViewHolder {
+        return FilmeViewHolder(
+            ItemListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int = filmesList.size
+
+    override fun onBindViewHolder(holder: FilmeViewHolder, position: Int) {
+        val filme = filmesList[position]
+        holder.binding.apply {
+            itemListImage.load("${BASE_URL_IMAGES}${filme.banner}")
+        }
+
+        holder.itemView.setOnClickListener {
+            onItemClickListener?.let {
+                it(filme)
+            }
+        }
+    }
+
+    fun setFilmeOnClickListener(listener: (FilmeView) -> Unit) {
+        onItemClickListener = listener
+    }
+
+    private var onItemClickListener: ((FilmeView) -> Unit)? = null
+}
---
+++ app/src/main/java/com/gabriel/themovie/ui/adapters/SerieAdapter.kt
@@ -0,0 +1,66 @@
+package com.gabriel.themovie.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.gabriel.themovie.databinding.ItemListBinding
+import com.gabriel.themovie.model.serie.model.SerieView
+import com.gabriel.themovie.util.constants.ConstantsView.BASE_URL_IMAGES
+
+class SerieAdapter : RecyclerView.Adapter<SerieAdapter.SerieViewHolder>() {
+
+    inner class SerieViewHolder(val binding: ItemListBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    private val differConfig = object : DiffUtil.ItemCallback<SerieView>() {
+        override fun areItemsTheSame(oldItem: SerieView, newItem: SerieView): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+
+        override fun areContentsTheSame(oldItem: SerieView, newItem: SerieView): Boolean {
+            return oldItem.id == newItem.id &&
+                    oldItem.title == newItem.title &&
+                    oldItem.description == newItem.description &&
+                    oldItem.banner == newItem.banner &&
+                    oldItem.background == newItem.background &&
+                    oldItem.nota == newItem.nota &&
+                    oldItem.favorito == newItem.favorito
+        }
+    }
+
+    private val differ = AsyncListDiffer(this, differConfig)
+
+    var seriesList: List<SerieView>
+        get() = differ.currentList
+        set(value) = differ.submitList(value)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SerieViewHolder {
+        return SerieViewHolder(
+            ItemListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int = seriesList.size
+
+    override fun onBindViewHolder(holder: SerieViewHolder, position: Int) {
+        val serie = seriesList[position]
+        holder.binding.apply {
+            itemListImage.load("${BASE_URL_IMAGES}${serie.banner}")
+        }
+
+        holder.itemView.setOnClickListener {
+            onItemClickListener?.let {
+                it(serie)
+            }
+        }
+    }
+
+    fun setFilmeOnClickListener(listener: (SerieView) -> Unit) {
+        onItemClickListener = listener
+    }
+
+    private var onItemClickListener: ((SerieView) -> Unit)? = null
+}
