469073c60ffcb2cea73131d9f6ad7db1f0661977
Gabriel <gnunes1909@gmail.com>
2022-09-07 14:24:50 -0300

    TM-13 feat: Finalizando as configurações do Navigation

--- app/src/main/java/com/gabriel/themovie/ui/activity/MainActivity.kt
+++ app/src/main/java/com/gabriel/themovie/ui/activity/MainActivity.kt
@@ -2,13 +2,31 @@ package com.gabriel.themovie.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.gabriel.themovie.R
+import com.gabriel.themovie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        inicializaView()
+    }
 
+    private fun inicializaView() {
+        navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigation.apply {
+            setupWithNavController(navController)
+            setOnNavigationItemReselectedListener {  }
+        }
     }
 }
